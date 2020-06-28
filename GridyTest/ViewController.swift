82cf54b185bb0f1277b3c7fc5690b7743ee55ef1
amoswk <36827671+amoswk@users.noreply.github.com>
2020-06-28 13:50:11 -0400

    adjusting bezier path lines

--- GridyTest/ViewController.swift
+++ GridyTest/ViewController.swift
@@ -9,50 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
-
     
-
+    
+    
+    
+    
     override func viewDidLoad() {
         let grid = GridView(frame: self.view.frame)
         self.view.addSubview(grid)
-  
+        
         // change background here
-         self.view.backgroundColor = UIColor.clear
+        self.view.backgroundColor = UIColor.clear
     }
-
-   
+    
+    
     
 }
 class GridView: UIView {
-  override func draw(_ rect: CGRect)
-                      {
-                       var bezierPath = UIBezierPath()
-                       bezierPath.lineWidth = 5.0
-                       for index in 1...3 {
-                           let start = CGPoint(x: CGFloat(index) * 50, y: 0)
-                           let end = CGPoint(x: CGFloat(index) * 50, y: self.bounds.height)
-                           let start2 = CGPoint(x: 0, y: CGFloat(index) * 50)
-                           let end2 = CGPoint(x: self.bounds.width, y: CGFloat(index) * 50)
-                           bezierPath.move(to: start)
-                           bezierPath.addLine(to: end)
-                           bezierPath.move(to: start2)
-                           bezierPath.addLine(to: end2)
-                           
-                       }
-
-                       bezierPath.close()
-                        
-                        
-                       UIColor.blue.setStroke()
-                        
-                    
-                        
-                       bezierPath.stroke()
-                       
-               
-                      }
+    
+    override func draw(_ rect: CGRect)
+    {
+        
+        var bezierPath = UIBezierPath()
+        bezierPath.lineWidth = 5.0
+        for index in 1...3 {
+            
+            let start = CGPoint(x: CGFloat(index) * 100, y: 0)
+            let end = CGPoint(x: CGFloat(index) * 100, y: self.bounds.height)
+            
+            let start2 = CGPoint(x: 0, y: CGFloat(index) * self.bounds.width/3)
+            let end2 = CGPoint(x: self.bounds.width, y: CGFloat(index) * self.bounds.width/3)
+            
+            
+            bezierPath.move(to: start)
+            bezierPath.addLine(to: end)
+            bezierPath.move(to: start2)
+            bezierPath.addLine(to: end2)
+            
+        }
+        
+        bezierPath.close()
+        
+        
+        UIColor.red.setStroke()
+        
+        
+        
+        bezierPath.stroke()
+        
+        
+    }
 }
 
 
