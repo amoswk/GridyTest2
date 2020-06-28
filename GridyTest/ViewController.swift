b99551d09abd7db43855734562f8c0595003f173
amoswk <36827671+amoswk@users.noreply.github.com>
2020-06-28 14:37:55 -0400

    fixed bezier path lines, added clear background

--- GridyTest/ViewController.swift
+++ GridyTest/ViewController.swift
@@ -16,49 +16,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         let grid = GridView(frame: self.view.frame)
+        grid.backgroundColor = UIColor.clear
         self.view.addSubview(grid)
         
         // change background here
-        self.view.backgroundColor = UIColor.clear
-    }
-    
-    
-    
-}
-class GridView: UIView {
-    
-    override func draw(_ rect: CGRect)
-    {
-        
-        var bezierPath = UIBezierPath()
-        bezierPath.lineWidth = 5.0
-        for index in 1...3 {
-            
-            let start = CGPoint(x: CGFloat(index) * 100, y: 0)
-            let end = CGPoint(x: CGFloat(index) * 100, y: self.bounds.height)
-            
-            let start2 = CGPoint(x: 0, y: CGFloat(index) * self.bounds.width/3)
-            let end2 = CGPoint(x: self.bounds.width, y: CGFloat(index) * self.bounds.width/3)
-            
-            
-            bezierPath.move(to: start)
-            bezierPath.addLine(to: end)
-            bezierPath.move(to: start2)
-            bezierPath.addLine(to: end2)
-            
-        }
-        
-        bezierPath.close()
-        
-        
-        UIColor.red.setStroke()
-        
-        
-        
-        bezierPath.stroke()
-        
         
     }
+    
+    
+    
 }
 
 
